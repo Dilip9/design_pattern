8eb0cbd9343cff8d7550b6d3a3c448e7543aefef
Dilip9 <dilip.verma940805@gmail.com>
2025-04-08 23:58:31 +0530

    Tripleton using round robin

--- src/main/java/org/example/singleton/Singleton.java
+++ src/main/java/org/example/singleton/Singleton.java
@@ -3,7 +3,22 @@ package org.example.singleton;
 public class Singleton {
 
     public static void main(String[] args) {
-        SingletonDesign singleton = SingletonDesign.getInstance();
-        System.out.println(singleton);
+        //SingletonDesign singleton = SingletonDesign.getInstance();
+        //System.out.println(singleton);
+        Tripleton tripleton = Tripleton.getInstance();
+        Tripleton tripleton2 = Tripleton.getInstance();
+        Tripleton tripleton3 = Tripleton.getInstance();
+        Tripleton tripleton4 = Tripleton.getInstance();
+        Tripleton tripleton5 = Tripleton.getInstance();
+        Tripleton tripleton6 = Tripleton.getInstance();
+        System.out.println(tripleton);
+        System.out.println(tripleton2);
+        System.out.println(tripleton3);
+        System.out.println("Instance 4 which need to be reuse"+tripleton4);
+        System.out.println(""+tripleton5);
+        System.out.println(tripleton6);
+
+
+
     }
 }
--- src/main/java/org/example/singleton/Tripleton.java
+++ src/main/java/org/example/singleton/Tripleton.java
@@ -1,4 +1,24 @@
 package org.example.singleton;
 
 public class Tripleton {
+    private static final int MAX_INSTANCES = 3;
+    private static final Tripleton[] instances = new Tripleton[MAX_INSTANCES];
+    private static int index =0;
+
+    private Tripleton() {
+        // Private constructor to prevent instantiation
+        System.out.println("Creating instance " + index);
+    }
+
+    static{
+        for (int i = 0; i < MAX_INSTANCES; i++) {
+            instances[i] = new Tripleton();
+        }
+    }
+
+    public static Tripleton getInstance(){
+        Tripleton instance = instances[index];
+        index = (index + 1) % MAX_INSTANCES;
+        return instance;
+    }
 }
